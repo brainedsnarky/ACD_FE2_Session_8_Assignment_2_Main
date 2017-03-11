4f9c539ac88020f16a5590c1973f2550da0e4243
Brainedsnarky <stead.official@gmail.com>
2017-03-11 07:24:19 -0800

    ini

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+   complexForm : FormGroup;
+   emailp = '^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)+(\.[a-z]{3,15})$';
+	 mobp = '[0-9]{10}';
+	 passp ='[a-z0-9]{7,20}';
+
+  constructor(fb: FormBuilder){
+    this.complexForm = fb.group({
+      'Username': [null, Validators.required] ,
+      'Email' : [null, Validators.compose([Validators.required, Validators.pattern(this.emailp)]) ],
+      'password' : [null, Validators.compose([Validators.required, Validators.pattern(this.passp)]) ],
+      'rpassword' : [null, Validators.compose([Validators.required, Validators.pattern(this.passp)]) ]
+    })
+  }
+
+  submitForm(value: any){
+    console.log(value);
+  }
+
 }
